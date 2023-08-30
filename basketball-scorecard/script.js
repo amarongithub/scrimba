b83506466b3b187e688f9e5d73edb0552e56d093
Amar0nGit <31379185+adewri@users.noreply.github.com>
2023-08-30 22:34:46 +0530

    Fixed JS bug calculating wrong score

--- basketball-scorecard/script.js
+++ basketball-scorecard/script.js
@@ -2,23 +2,29 @@ let homeScore = document.querySelector(".home-score");
 let guestScore = document.querySelector(".guest-score");
 let previousScore = document.querySelector(".previous-score");
 
-let score = 0;
+let homeScoreInitial = 0;
+let guestScoreInitial = 0;
 
-function scoreIncrement(num) {
-  return (score += num);
+function homeIncrement(num) {
+  return (homeScoreInitial += num);
+}
+
+function guestIncrement(num) {
+  return (guestScoreInitial += num);
 }
 
 function homeScoreIncrement(num) {
-  homeScore.textContent = scoreIncrement(num);
+  homeScore.textContent = homeIncrement(num);
 }
 
 function guestScoreIncrement(num) {
-  guestScore.textContent = scoreIncrement(num);
+  guestScore.textContent = guestIncrement(num);
 }
 
 function resetScore() {
   previousScore.textContent = `Previous Score - Home: ${homeScore.textContent} Guest: ${guestScore.textContent}`;
-  score = 0;
-  homeScore.textContent = score;
-  guestScore.textContent = score;
+  homeScoreInitial = 0;
+  guestScoreInitial = 0;
+  homeScore.textContent = homeScoreInitial;
+  guestScore.textContent = guestScoreInitial;
 }
